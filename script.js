022b8e7916e1dbbfe88c5d41cebf23e62a027656
mselvadevi <selvakumar_m2@preludesys.com>
2023-01-23 13:37:13 +0530

    Updated with class

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+ var Name = "kumar";
+
+const App = function(){
+    this.Name = 'Selva App';
+    this.ShowName = () => {
+        console.log(`From App ${this.Name}`);
+    }
+}
+
+ let appObject = new App();
+ appObject.ShowName();
+
+const AppLiteral = {
+    Name: 'Selva AppLiteral',
+    ShowName : function() {
+        console.log(`from AppLiteral ${this.Name}`)
+    }
+}
+
+AppLiteral.ShowName();
+
+const AppSingleton = new function(){
+    this.Name = "Selva AppSingleton";
+    this.ShowName = () => {
+        console.log(`from app singleton ${this.Name}`)
+    }
+}
+
+AppSingleton.ShowName();
